63a3e745af3b62b262a321a093f4feb9f7bb4802
Mikhail Titov <mikhail.a.titov@gmail.com>
2023-02-14 01:00:04 -0500

    introduced multi-tutorial setup

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,29 @@
 
 RCT_BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )/../" &> /dev/null && pwd 2> /dev/null; )"
 
+while getopts ":n:t:" option; do
+   case $option in
+      n) # tutorial name
+         NAME=$OPTARG;;
+      t) # tutorial tag
+         TAG=$OPTARG;;
+     \?) # unknown option
+         echo "Unknown option $OPTARG";;
+   esac
+done
+
+if [[ -z $NAME ]]; then
+    NAME="${RCT_TUTORIAL_NAME}"
+fi
+
+if [[ -z $TAG ]]; then
+    TAG="${RCT_TUTORIAL_TAG:-latest}"
+fi
+TAG="radicalcybertools/tutorials:$TAG"
+
+echo "Build docker container $TAG (name: ${NAME:-n/a})"
 docker build \
-    -t radicalcybertools/tutorials \
+    -t "$TAG" \
+    --build-arg TUTORIAL_NAME="$NAME"\
     -f "$RCT_BASE_DIR/docker/Dockerfile" \
     "$RCT_BASE_DIR/tutorials/"
