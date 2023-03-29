f873f090f2049efb7bc452495b5f069134488be7
Mikhail Titov <mikhail.a.titov@gmail.com>
2023-03-29 18:35:09 -0400

    updates for the tutorial development

--- docker/build.sh
+++ docker/build.sh
@@ -2,12 +2,14 @@
 
 RCT_BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )/../" &> /dev/null && pwd 2> /dev/null; )"
 
-while getopts ":n:t:" option; do
+while getopts ":n:t:p:" option; do
    case $option in
       n) # tutorial name
          NAME=$OPTARG;;
       t) # tutorial tag
          TAG=$OPTARG;;
+      p) # build platform
+         PLATFORM=$OPTARG;;
      \?) # unknown option
          echo "Unknown option $OPTARG";;
    esac
@@ -22,10 +24,15 @@ if [[ -z $TAG ]]; then
 fi
 TAG="radicalcybertools/tutorials:$TAG"
 
+if [[ -z $PLATFORM ]]; then
+    PLATFORM="linux/amd64"
+fi
+
 echo "Build docker container $TAG (name: ${NAME:-n/a})"
 docker build \
     -t "$TAG" \
-    --build-arg TUTORIAL_NAME="$NAME"\
+    --build-arg TUTORIAL_NAME="$NAME" \
+    --build-arg BUILDPLATFORM="$PLATFORM" \
     -f "$RCT_BASE_DIR/docker/Dockerfile" \
     "$RCT_BASE_DIR/src/"
 
