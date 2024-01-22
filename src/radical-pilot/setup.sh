0667b38ec01fb47c5abe52d70ff3bca5a8a3b4bd
Mikhail Titov <mikhail.a.titov@gmail.com>
2024-01-22 14:24:24 -0500

    updated `setup.sh` for RP tutorial

--- src/radical-pilot/setup.sh
+++ src/radical-pilot/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ensure that necessary package is installed
+sudo apt-get update  -y && \
+sudo apt-get install -y curl
+
 RADICAL_PILOT_TARBALL_URL="\
 https://api.github.com/repos/radical-cybertools/radical.pilot/tarball/master"
 
