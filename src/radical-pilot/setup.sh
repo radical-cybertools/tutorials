e9c299073391d8f6cc40e28f1a8bd42be1bfc13f
Mikhail Titov <mikhail.a.titov@gmail.com>
2024-01-22 14:58:26 -0500

    rollback for `setup.sh` in the RP tutorial

--- src/radical-pilot/setup.sh
+++ src/radical-pilot/setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo_cmd=$(which sudo)
-
-# ensure that necessary package is installed
-$sudo_cmd apt-get update  -y && \
-$sudo_cmd apt-get install -y curl
-
 RADICAL_PILOT_TARBALL_URL="\
 https://api.github.com/repos/radical-cybertools/radical.pilot/tarball/master"
 
